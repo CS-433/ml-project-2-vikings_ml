c26a4580ad8a35a258775d3448efa35e91d111d4
benjaminrike1 <benjaminrike1@gmail.com>
2021-11-19 22:22:03 +0100

    Cleaning

--- baseline.py
+++ baseline.py
@@ -72,24 +72,6 @@ def error_rate(predictions, labels):
         predictions.shape[0])
 
 
-# Write predictions from neural network to a file
-def write_predictions_to_file(predictions, labels, filename):
-    max_labels = numpy.argmax(labels, 1)
-    max_predictions = numpy.argmax(predictions, 1)
-    file = open(filename, "w")
-    n = predictions.shape[0]
-    for i in range(0, n):
-        file.write(max_labels(i) + ' ' + max_predictions(i))
-    file.close()
-
-
-# Print predictions from neural network
-def print_predictions(predictions, labels):
-    max_labels = numpy.argmax(labels, 1)
-    max_predictions = numpy.argmax(predictions, 1)
-    print(str(max_labels) + ' ' + str(max_predictions))
-
-
 # Convert array of labels to an image
 def label_to_img(imgwidth, imgheight, w, h, labels):
     array_labels = numpy.zeros([imgwidth, imgheight])
