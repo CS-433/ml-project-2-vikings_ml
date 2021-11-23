ad35919862b177211e8f1e16b3ba4f7020634c0e
benjaminrike1 <benjaminrike1@gmail.com>
2021-11-23 10:11:26 +0100

    Minor cfg fix

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 NUM_CHANNELS = 3  # RGB images
 PIXEL_DEPTH = 255
 NUM_LABELS = 2
-TRAINING_SIZE = 1600
+TRAINING_SIZE = 1700
 VALIDATION_SIZE = 5  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
 BATCH_SIZE = 16  # 64
 NUM_EPOCHS = 100    
-RESTORE_MODEL = True  # If True, restore existing model instead of training a new one
+RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
 RECORDING_STEP = 0
 
 # Set image patch size in pixels
