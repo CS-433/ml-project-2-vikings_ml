a4a314fe50a678283c08f11ebc3425038eee93aa
benjaminrike1 <benjaminrike1@gmail.com>
2021-11-24 11:37:58 +0100

    Minor

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ NUM_LABELS = 2
 TRAINING_SIZE = 1700
 VALIDATION_SIZE = 5  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
-BATCH_SIZE = 16  # 64
+BATCH_SIZE = 64
 NUM_EPOCHS = 100    
-RESTORE_MODEL  = True  # If True, restore existing model instead of training a new one
+RESTORE_MODEL  = False  # If True, restore existing model instead of training a new one
 RECORDING_STEP = 0
 
 # Set image patch size in pixels
